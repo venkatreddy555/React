a436c245622cc15e31a8ffc5631fd9cdf6b30b83
sunkireddy555 <sunkireddy555@gmail.com>
2021-06-03 10:07:35 +0530

    Initial classes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// JSX
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const app = document.getElementById('app');
+
+function Awesome() {
+    return <>
+        <h1>Awesome</h1>
+        <button>LOL</button>
+        <a href="https://www.golang.org">Go</a>
+    </>
+}
+
+// Components
+const title = "Programming with Go";
+let x  = 1;
+function Amazing() {
+    return <>
+        <h1>Amazing {title} {1+1} { function wow(data){
+            alert(data);
+            x = x+1;
+        }(x)}</h1>
+        <button>Click</button>
+        <a href="https://www.rust-lang.org">Go</a>
+    </>
+}
+
+
+class Good extends React.Component {
+
+    render(){
+        return <>
+            <h1>Good Component</h1>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            Apple
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    }
+}
+
+function Root() {
+    return <React.Fragment>
+        <h1>Lol!!</h1>
+        <Good/>
+        <Amazing />
+        <Awesome />
+        <Amazing />
+        
+        <Awesome />
+        <Amazing />
+        <Awesome />
+        <Amazing />
+        <Awesome />
+
+    </React.Fragment>
+}
+
+
+
+ReactDOM.render( <Root/>, app);

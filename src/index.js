21ff42665137fc8f673c69538b27d3886b7a5701
sunkireddy555 <sunkireddy555@gmail.com>
2021-06-08 10:15:26 +0530

    set stateconcept

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ let x  = 1;
 function Amazing() {
     return <>
         <h1>Amazing {title} {1+1} { function wow(data){
-            alert(data);
             x = x+1;
         }(x)}</h1>
         <button>Click</button>
@@ -29,7 +28,36 @@ function Amazing() {
 
 
 class Good extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            x: 10,
+            y: 90,
+            country: "IND",
+            items: [],
+            person: {
+                name: "",
+                age: "",
+            }
+        }
+    }
 
+    handleClick = () => {
+        this.setState({
+            x: this.state.x + 1,
+            y: this.state.y + 2
+        });
+    }
+
+    handleSelect = (event) => {
+        // const { currentTarget } = event;
+        // const { value} = currentTarget;
+        const { currentTarget: { value } } = event;
+        this.setState({country: value}, () => {
+            console.log(this.state.country);
+        });
+        
+    }
     render(){
         return <>
             <h1>Good Component</h1>
@@ -37,7 +65,30 @@ class Good extends React.Component {
                 <tbody>
                     <tr>
                         <td>
-                            Apple
+                            Apple { this.state.x}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Y { this.state.y}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h1>{this.state.country}</h1>
+                        </td>
+                        <td>
+                            <select onChange={this.handleSelect}>
+                                <option value="IND">IND</option>
+                                <option value="NZ">NZ</option>
+                                <option value="AUS">AUS</option>
+                                <option value="ENG">ENG</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                           <button onClick={this.handleClick}>Click</button>
                         </td>
                     </tr>
                 </tbody>
